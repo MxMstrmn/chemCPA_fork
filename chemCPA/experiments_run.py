f360deb9e265a8bff36e9a26a00712e4fe91d3a4
Keming Zhang <kemingzhang@link.cuhk.edu.cn>
2024-02-15 09:14:20 +0100

    update hydra configuration

--- chemCPA/experiments_run.py
+++ chemCPA/experiments_run.py
@@ -17,10 +17,10 @@ from lightning.pytorch import seed_everything
 from pytorch_lightning.loggers import WandbLogger
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
-@hydra.main(config_path='../chemCPA/config', config_name='config')
+@hydra.main(version_base=None, config_path='../chemCPA/config', config_name='config')
 def main(cfg: DictConfig):
     dm = DataModule(cfg.model.hparams.batch_size,
-                    cfg.model.training_params.full_eval_during_train,
+                    cfg.train.full_eval_during_train,
                     **cfg.dataset.data_params
                     )
 
@@ -30,44 +30,38 @@ def main(cfg: DictConfig):
         dm.datasets['training'].num_knockouts,
         dm.datasets['training'].num_covariates,
         cfg.model.hparams,
-        cfg.model.training_params,
-        cfg.model.test_params,
+        cfg.train,
+        cfg.test,
         **cfg.model.additional_params,
         drug_embedding_dimension=dm.datasets['training'].drug_embedding_dimension,
         knockout_embedding_dimension=dm.datasets['training'].knockout_embedding_dimension
         )
-    
+
     early_stop_callback = EarlyStopping('average_r2_score', 
-                                    patience=cfg.model.training_params.patience, 
+                                    patience=cfg.train.patience, 
                                     mode='max')
 
-    if (not cfg.model.training_params.run_eval_disentangle) and (not cfg.model.test_params.run_eval_disentangle):
-        trainer = L.Trainer(
-            logger=WandbLogger(log_model="all"),
-            max_epochs=cfg.model.training_params.num_epochs,
-            max_time=cfg.model.training_params.max_time,
-            check_val_every_n_epoch= cfg.model.training_params.checkpoint_freq,
-            default_root_dir=cfg.model.save_dir,
-            profiler="advanced",
-            callbacks=[early_stop_callback],
-            #inference_mode=False
-        )
-    else: 
-        trainer = L.Trainer(
-            logger=WandbLogger(project='CPA', log_model="all", save_dir=cfg.model.save_dir),
-            max_epochs=cfg.model.training_params.num_epochs,
-            max_time=cfg.model.training_params.max_time,
-            check_val_every_n_epoch= cfg.model.training_params.checkpoint_freq,
-            default_root_dir=cfg.model.save_dir,
-            profiler="advanced",
-            callbacks=[early_stop_callback],
-            inference_mode=False
-        )
+    wandb_logger = WandbLogger(
+        project = cfg.model.model_type + "_" + cfg.dataset.dataset_type,
+        save_dir = cfg.model.save_dir      
+    )
 
-
+    inference_mode = ((not cfg.train.run_eval_disentangle) and (not cfg.test.run_eval_disentangle))
+    trainer = L.Trainer(
+        logger=wandb_logger,
+        max_epochs=cfg.train.num_epochs,
+        max_time=cfg.train.max_time,
+        check_val_every_n_epoch= cfg.train.checkpoint_freq,
+        default_root_dir=cfg.model.save_dir,
+        profiler="advanced",
+        callbacks=[early_stop_callback],
+        inference_mode=inference_mode
+    )
+    
     trainer.fit(model, datamodule=dm)
     trainer.test(model, datamodule=dm)
-
+    
+    
 if __name__ == "__main__":
     main()
 
